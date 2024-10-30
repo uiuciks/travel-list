e0669fde01cb74faf5136030fde78dbfb6ffa8db
Suci Widhi <uiuciks@gmail.com>
2024-10-30 15:40:18 +0800

    New Components

---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Logo() {
+  return <h1>🏝️ Ucik The Explorer 🧳</h1>;
+}
---
+++ src/components/Stats.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Stats() {
+  return (
+    <footer>
+      <em>You have x items on your list, and you already packed x (X%)</em>
+    </footer>
+  );
+}
